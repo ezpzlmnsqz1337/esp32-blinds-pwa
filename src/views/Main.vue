31320c8e71c4adc11dc7f22478a2d6f9fc5ba7c3
ezpzlmnsqz1337 <ezpzlmnsqz1337@github.com>
2020-11-17 23:42:34 +0100

    Configured formatters

---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-fluid">
+    <h1>Blinds control</h1>
+    <Balcony />
+    <Window />
+  </div>
+</template>
+
+<script>
+import Balcony from '@/components/Balcony'
+import Window from '@/components/Window'
+
+export default {
+  name: 'Main',
+  components: {
+    Balcony,
+    Window
+  }
+}
+</script>
